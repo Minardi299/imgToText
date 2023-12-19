4848923cac9a47ce8b959d3938064d8de108ba48
Minardi299 <phu.ngo@outlook.com>
2023-12-18 20:40:09 -0500

    installing langchain

--- ImgConvert.py
+++ ImgConvert.py
@@ -1,18 +1,19 @@
 import pytesseract
-import cv2
+from langchain.llms import Ollama
+# import cv2
 import tkinter as tk
-from tkinter import filedialog
-root = tk.Tk()
-root.withdraw()
+# from tkinter import filedialog
+# root = tk.Tk()
+# root.withdraw()
 
-file_path = filedialog.askopenfilename()
+# file_path = filedialog.askopenfilename()
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\Tesseract-OCR\\tesseract.exe'
-img =cv2.imread(file_path)
-
-text =pytesseract.image_to_string(img)  # this will print the text present in the image
+# img =cv2.imread(file_path)
+ollama = Ollama(base_url='http://localhost:11434',model = 'llama2')
+print(ollama('why is the sky blue?'))
+text =pytesseract.image_to_string('test1.jpeg')  # this will print the text present in the image
 with open ("result.txt","a")as file:
 
     file.write('==============New Image==============\n')
-    file.write(f'Image:{file_path}\n')
+    file.write(f'Image:\n')
     file.write(text)
-    
